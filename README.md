1e614a1c7f9b2d1b227a2bdf30e55601c165dd02
Nathan Lee <esoodev@gmail.com>
2017-08-28 13:52:42 +0900

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # eve-sso-simple
 Simple promise-based node.js module simplifying EVE Online's SSO authentication flow.
+Please email me for bugs/suggestions! 
+
+## Usage
+```
+router.get('/', (req, res) => {
+    
+    async function login(req, res) {
+
+        var tokens = await loginService.login(  
+            // Tokens (JWT) encoded using 'token_secret,' then saved as cookies.
+            // Redirects to SSO login page if not logged in.
+            {
+                // Fill in below the application details registered on the dev page.
+                client_id: ...,         
+                client_secret: ..., 
+                redirect_uri: ...,   
+                scope: ...,                 
+                token_secret: 'secret'  // For jwt encoding.
+            }
+            , req, res);
+
+        if (tokens)
+            res.send(tokens);
+    };
+
+    login(req, res);
+
+});
+```
---
+++ eve-sso-simple.js
@@ -0,0 +1,87 @@
+﻿var qs = require('querystring');
+var request = require('request');
+var jwt = require('jwt-simple');
+
+module.exports = {
+
+    // Send a request to ESI
+    login: function (setup, req, res) {
+
+        return new Promise((resolve, reject) => {
+
+            var accessToken;
+            var charToken;
+
+            if (req.query.code != null) {
+
+                request(
+                    {
+                        url: 'https://login.eveonline.com/oauth/token',
+                        method: 'POST',
+                        headers: {
+                            'Authorization': 'Basic ' + new Buffer(setup.client_id + ':' + setup.client_secret).toString('base64'),
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Host': 'login.eveonline.com'
+                        },
+                        body: qs.stringify({
+                            "grant_type": "authorization_code",
+                            "code": req.query.code
+                        })
+                    }, function (tokenErr, tokenRes, tokenBody) {
+
+                        if (tokenRes && (tokenRes.statusCode === 200 || tokenRes.statusCode === 201)) {
+
+                            accessToken = JSON.parse(tokenBody);
+                            res.cookie('access_token', jwt.encode(accessToken, setup.token_secret));
+
+                            request(
+                                {
+                                    url: 'https://login.eveonline.com/oauth/verify',
+                                    method: 'GET',
+                                    headers: {
+                                        'User-Agent': req.headers['user-agent'],
+                                        'Authorization': accessToken.token_type + ' ' + accessToken.access_token,
+                                        'Host': 'login.eveonline.com'
+                                    }
+                                }, function (charErr, charRes, charBody) {
+
+                                    if (charRes && (charRes.statusCode === 200 || charRes.statusCode === 201)) {
+                                        
+                                        charToken = JSON.parse(charBody);
+                                        res.cookie('character_token', jwt.encode(charToken, setup.token_secret));
+
+                                    }
+
+                                    resolve({
+                                        access_token: accessToken,
+                                        character_token: charToken
+                                    });
+                                });
+                        }
+                    });
+            } else if (req.cookies.access_token != null || req.cookies.character_token != null) {
+                resolve({
+                    access_token: jwt.decode(req.cookies.access_token, setup.token_secret),
+                    character_token: jwt.decode(req.cookies.character_token, setup.token_secret)
+                });
+            } else {
+
+                var query = qs.stringify(
+                    {
+                        response_type: "code",
+                        redirect_uri: setup.redirect_uri,
+                        client_id: setup.client_id,
+                        scope: setup.scope
+                    }
+                );
+
+                res.redirect('https://login.eveonline.com/oauth/authorize/?' + query);
+            }
+
+
+        });
+
+
+    }
+
+}
